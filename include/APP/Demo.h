301f7903a9849287ec20b28b5b72ecdb46dcc35c
Ahmed Adel <ahmed.adel5abdalla@gmail.com>
2024-08-04 01:45:57 -0700

    Update

---
+++ include/APP/Demo.h
@@ -0,0 +1,28 @@
+#ifndef DEMO1_H_
+#define DEMO1_H_
+
+#include"Sched.h"
+
+/********************Stop Watch Buttons***********************/
+typedef enum 
+{
+    DefaultView,
+    ModeView,
+    StopWatchView,
+    EditTimeView
+}WatchViews;
+
+/********************Stop Watch Buttons***********************/
+
+void WatchInit(void);
+
+
+
+
+
+
+
+
+
+#endif
+
